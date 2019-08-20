f618e0fdf4c179cc9dc249ebe102551e291d1c17
Jeremy Green <hello@jeremy.green>
2019-08-19 20:13:12 -0400

    refactor: add nodemon, add timestamp, remove unused config

--- services/switches/controllers/index.js
+++ services/switches/controllers/index.js
@@ -2,4 +2,4 @@ const uuid = require('uuid/v4');
 
 const switches = require('../models');
 
-module.exports = io => io.on('connection', socket => switches.forEach(s => s.on('change', status => socket.emit('change', { ...status, id: uuid() }))));
+module.exports = io => io.on('connection', socket => switches.forEach(s => s.on('change', status => socket.emit('change', { ...status, id: uuid(), ts: Date.now() }))));
