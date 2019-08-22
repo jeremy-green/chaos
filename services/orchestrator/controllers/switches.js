e18e4c4707540f5015d698a348786c1fb481a1f2
Jeremy Green <hello@jeremy.green>
2019-08-21 20:41:09 -0400

    refactor: remove unused controllers

--- services/orchestrator/controllers/switches.js
+++
@@ -1,6 +0,0 @@
-const client = require('socket.io-client');
-
-const namespace = 'switches';
-const socket = client(`http://localhost:83/${namespace}`);
-
-module.exports = socket;
--- services/orchestrator/controllers/weather.js
+++
@@ -1,6 +0,0 @@
-const client = require('socket.io-client');
-
-const namespace = 'weather';
-const socket = client(`http://localhost:81/${namespace}`);
-
-module.exports = socket;
