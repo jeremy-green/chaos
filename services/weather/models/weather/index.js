7a6b9b94b25f632450677263c75e3e284251281f
Jeremy Green <hello@jeremy.green>
2019-09-13 18:24:12 -0400

    refactor: change things

--- services/weather/models/weather/index.js
+++ services/weather/models/weather/index.js
@@ -4,8 +4,6 @@ const config = require('config');
 const fetch = require('node-fetch');
 const { Machine, interpret, assign } = require('xstate');
 
-const { states, actions } = require('./state');
-
 const { insert } = require('../datastore');
 
 const eventEmitter = new EventEmitter();
@@ -14,18 +12,49 @@ const {
   apiKey, latlng, maxCallsPerDay, precipProbabilityThreshold,
 } = config;
 
-const milliseconds = 1000;
-const seconds = 60;
-const minutes = 60;
-const hours = 24;
-const interval = Math.floor((hours * minutes * seconds * milliseconds) / maxCallsPerDay);
-
 class Weather {
-  // @todo I don't love this...                              👇
-  static #machine = interpret(new Machine({ ...states }, { ...actions(Weather.emit) })).start();
+  static #service = interpret(
+    new Machine(
+      {
+        id: 'weather',
+        initial: 'boot',
+        states: {
+          boot: {
+            on: {
+              PRECIPITATION: { target: 'precipitation', actions: ['notify'] },
+              NO_PRECIPITATION: { target: 'no_precipitation', actions: ['notify'] },
+              REJECTED: { target: 'rejected', actions: ['rejected'] },
+            },
+          },
+          no_precipitation: {
+            on: {
+              PRECIPITATION: { target: 'precipitation', actions: ['notify'] },
+              REJECTED: { target: 'no_precipitation', actions: ['rejected'] },
+            },
+          },
+          precipitation: {
+            on: {
+              NO_PRECIPITATION: { target: 'no_precipitation', actions: ['notify'] },
+              REJECTED: { target: 'precipitation', actions: ['rejected'] },
+            },
+          },
+        },
+      },
+      {
+        actions: {
+          notify(context, { status, currently }) {
+            Weather.emit('precipitation', { status, currently });
+          },
+          rejected(context, event) {
+            console.log('rejected', context, event);
+          },
+        },
+      },
+    ),
+  ).start();
 
   static update(name, data) {
-    this.#machine.send(name.toUpperCase(), { ...data });
+    this.#service.send(name.toUpperCase(), { ...data });
   }
 
   static save(data) {
@@ -46,57 +75,64 @@ class Weather {
 }
 
 interpret(
-  new Machine({
-    id: 'service',
-    context: {
-      date: new Date().getDate(),
-      index: maxCallsPerDay,
-      status: null,
-    },
-    initial: 'idle',
-    states: {
-      idle: {
-        after: {
-          [interval]: 'loading',
-        },
+  new Machine(
+    {
+      id: 'service',
+      context: {
+        date: new Date().getDate(),
+        index: maxCallsPerDay,
+        status: null,
       },
-      loading: {
-        invoke: {
-          async src({ date, index }) {
-            const { currently } = await Weather.call(apiKey, latlng);
-            const { precipProbability } = currently;
-
-            const isPrecipitating = precipProbability > precipProbabilityThreshold;
-            const status = isPrecipitating ? 'precipitation' : 'no_precipitation';
-
-            const todaysDate = new Date().getDate();
-            const indexDate = date !== todaysDate
-              ? { index: maxCallsPerDay, date: todaysDate }
-              : { index: index - 1, date };
-
-            console.log(indexDate, currently, status);
-
-            return { ...indexDate, status, currently };
-          },
-          onDone: {
-            target: 'idle',
-            actions: [
-              assign({
-                index: (context, { data: { index } }) => index,
-                date: (context, { data: { date } }) => date,
-              }),
-              (context, { data: { status, currently } }) => Weather.update(status, { status, currently }),
-              (context, { data: { currently } }) => Weather.save(currently),
-            ],
-          },
-          onError: {
-            target: 'idle',
-            actions: () => console.log('error'),
+      initial: 'idle',
+      states: {
+        idle: {
+          after: {
+            INTERVAL: 'loading',
           },
         },
+        loading: {
+          invoke: {
+            async src({ date, index }) {
+              const { currently } = await Weather.call(apiKey, latlng);
+              const { precipProbability } = currently;
+
+              const isPrecipitating = precipProbability > precipProbabilityThreshold;
+              const status = isPrecipitating ? 'precipitation' : 'no_precipitation';
+
+              const todaysDate = new Date().getDate();
+              const indexDate = date !== todaysDate
+                ? { index: maxCallsPerDay, date: todaysDate }
+                : { index: index - 1, date };
+
+              console.log(indexDate, currently, status);
+
+              return { ...indexDate, status, currently };
+            },
+            onDone: {
+              target: 'idle',
+              actions: [
+                assign({
+                  index: (context, { data: { index } }) => index,
+                  date: (context, { data: { date } }) => date,
+                }),
+                (context, { data: { status, currently } }) => Weather.update(status, { status, currently }),
+                (context, { data: { currently } }) => Weather.save(currently),
+              ],
+            },
+            onError: {
+              target: 'idle',
+              actions: () => console.log('error'),
+            },
+          },
+        },
       },
     },
-  }),
+    {
+      delays: {
+        INTERVAL: Math.floor((24 * 60 * 60 * 1000) / maxCallsPerDay),
+      },
+    },
+  ),
 ).start();
 
 module.exports = Weather;
