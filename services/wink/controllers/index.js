abf76228d57bcb55b0aef23e7a0038504e5e4f44
Jeremy Green <hello@jeremy.green>
2019-09-21 09:41:50 -0400

    refactor: remove unused code

--- services/wink/controllers/index.js
+++ services/wink/controllers/index.js
@@ -2,18 +2,18 @@ const { powerDevices, updateDevices } = require('../models');
 
 module.exports = (io) => {
   io.on('connection', async (socket) => {
-    socket.on('power', (settings) => {
-      const { state, id } = settings;
-      const devices = powerDevices[state];
-      console.log('power', devices.map(({ name }) => name).join(','), state, id);
-      devices.forEach(device => device.turn(state));
-    });
-
-    socket.on('update', (settings) => {
-      const { state, id } = settings;
-      const devices = updateDevices[state];
-      console.log('update', devices.map(({ name }) => name).join(','), state, id);
-      // devices.forEach(device => deviceMap[device].set(state));
-    });
+    socket
+      .on('power', (settings) => {
+        const { state, id } = settings;
+        const devices = powerDevices[state];
+        console.log('power', devices.map(({ name }) => name).join(','), state, id);
+        devices.forEach(device => device.turn(state));
+      })
+      .on('update', (settings) => {
+        const { state, id } = settings;
+        const devices = updateDevices[state];
+        console.log('update', devices.map(({ name }) => name).join(','), state, id);
+        // devices.forEach(device => device.set(state));
+      });
   });
 };
