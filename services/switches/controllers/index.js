a92a974daf9b1fffd1a54ab72b8ca67351c9f50d
Jeremy Green <hello@jeremy.green>
2019-09-21 09:41:15 -0400

    refactor: remove unused code

--- services/switches/controllers/index.js
+++ services/switches/controllers/index.js
@@ -3,11 +3,5 @@ const uuid = require('uuid/v4');
 const switches = require('../models');
 
 module.exports = io => io.on('connection', (socket) => {
-  socket.on('toggle', (data) => {
-    const { name, state } = data;
-    const [device] = switches.filter(s => s.name === `${name} Home` /* 😒 */);
-    device.handler(state);
-  });
-
   switches.forEach(s => s.on('toggle', state => socket.emit('toggle', { ...state, id: uuid(), ts: Date.now() })));
 });
