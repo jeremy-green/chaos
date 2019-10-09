51f2af7c9997954ae7e4c91c83c731547ea1bf0c
Jeremy Green <hello@jeremy.green>
2019-10-09 18:08:51 -0400

    refactor: move machine

--- services/weather/models/index.js
+++ services/weather/models/index.js
@@ -1,3 +1,73 @@
+const config = require('config');
+
+const {
+  Machine,
+  interpret,
+  assign,
+  actions: { log },
+} = require('xstate');
+
 const Weather = require('./weather');
+
+const precipitationMap = { true: 'precipitation', false: 'no_precipitation' };
+
+const {
+  apiKey, latlng, maxCallsPerDay, precipProbabilityThreshold,
+} = config;
+
+interpret(
+  new Machine(
+    {
+      id: 'service',
+      context: {
+        status: null,
+      },
+      initial: 'wait',
+      states: {
+        wait: {
+          after: {
+            INTERVAL: 'load',
+          },
+        },
+        load: {
+          invoke: {
+            src: () => Weather.call(apiKey, latlng),
+            onDone: { target: 'update', actions: ['updateStatus', 'logResponse'] },
+            onError: { target: 'wait', actions: ['logError'] },
+          },
+        },
+        update: {
+          on: {
+            '': {
+              target: 'wait',
+              actions: ['sendStatusUpdate', 'saveResponse'],
+            },
+          },
+        },
+      },
+    },
+    {
+      actions: {
+        sendStatusUpdate: ({ status }, { data: { currently } }) => Weather.update(status, { status, currently }),
+        saveResponse: (context, { data: { currently } }) => Weather.save(currently),
+        logResponse: log(({ status }, { data: { currently } }) => ({ status, currently })),
+        logError: log((context, event) => ({ context, event })),
+        updateStatus: assign({
+          status: (
+            context,
+            {
+              data: {
+                currently: { precipProbability },
+              },
+            },
+          ) => precipitationMap[precipProbability >= precipProbabilityThreshold],
+        }),
+      },
+      delays: {
+        INTERVAL: Math.floor((24 * 60 * 60 * 1000) / maxCallsPerDay),
+      },
+    },
+  ),
+).start();
 
 module.exports = { Weather };
--- services/weather/models/weather/index.js
+++ services/weather/models/weather/index.js
@@ -1,21 +1,14 @@
 const EventEmitter = require('events');
 
-const config = require('config');
 const fetch = require('node-fetch');
 
 const {
-  Machine, interpret, assign, actions,
+  Machine,
+  interpret,
+  actions: { log },
 } = require('xstate');
 
 const { insert } = require('../datastore');
-
-const { log } = actions;
-
-const precipitationMap = { true: 'precipitation', false: 'no_precipitation' };
-
-const {
-  apiKey, latlng, maxCallsPerDay, precipProbabilityThreshold,
-} = config;
 
 const eventEmitter = new EventEmitter();
 
@@ -76,60 +69,5 @@ class Weather {
     return fetch(`https://api.darksky.net/forecast/${key}/${loc}`).then(res => res.json());
   }
 }
-
-interpret(
-  new Machine(
-    {
-      id: 'service',
-      context: {
-        status: null,
-      },
-      initial: 'wait',
-      states: {
-        wait: {
-          after: {
-            INTERVAL: 'load',
-          },
-        },
-        load: {
-          invoke: {
-            src: () => Weather.call(apiKey, latlng),
-            onDone: { target: 'update', actions: ['updateStatus', 'logResponse'] },
-            onError: { target: 'wait', actions: ['logError'] },
-          },
-        },
-        update: {
-          on: {
-            '': {
-              target: 'wait',
-              actions: ['sendStatusUpdate', 'saveResponse'],
-            },
-          },
-        },
-      },
-    },
-    {
-      actions: {
-        sendStatusUpdate: ({ status }, { data: { currently } }) => Weather.update(status, { status, currently }),
-        saveResponse: (context, { data: { currently } }) => Weather.save(currently),
-        logResponse: log(({ status }, { data: { currently } }) => ({ status, currently })),
-        logError: log((context, event) => ({ context, event })),
-        updateStatus: assign({
-          status: (
-            context,
-            {
-              data: {
-                currently: { precipProbability },
-              },
-            },
-          ) => precipitationMap[precipProbability >= precipProbabilityThreshold],
-        }),
-      },
-      delays: {
-        INTERVAL: Math.floor((24 * 60 * 60 * 1000) / maxCallsPerDay),
-      },
-    },
-  ),
-).start();
 
 module.exports = Weather;
